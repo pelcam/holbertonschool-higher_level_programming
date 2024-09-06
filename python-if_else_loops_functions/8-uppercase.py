309b6ad924c32ef8837db62e4140ba6ca5745c7b
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-06 17:10:29 +0000

    updating file for task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            upper_char = chr(ord(char) - 32)
+        else:
+            upper_char = char
+        print("{}".format(upper_char), end="")
+    print()
