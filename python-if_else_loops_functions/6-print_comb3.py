bb064fcf9f18d4fba47293f2c5eb7f4bfa6658d8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-06 09:00:05 +0000

    updating file 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,10 @@
-#
+#!/usr/bin/python3
+for i in range(0, 90):
+    dizain = i//10
+    lastdigit = i % 10
+    if lastdigit == 0:
+        i += (3 + dizain)
+    if i < 89:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
