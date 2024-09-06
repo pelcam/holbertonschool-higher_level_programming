13231a7eefd728fc2b7dffda7cabe4ab3e0557ae
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-06 17:38:21 +0000

    updating file for task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
-i
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print(f"{num_args} arguments.")
+    elif num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
