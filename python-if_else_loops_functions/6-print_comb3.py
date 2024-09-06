0a4e6e781a929996f81b2e3e058361a7a8abb4bb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-06 16:57:23 +0000

    updating flle for task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 90):
-    dizain = i//10
-    lastdigit = i % 10
-    if lastdigit == 0:
-        i += (3 + dizain)
-    if i < 89:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
