8fe0f80acf2db7df79666e8a15802293cd4c0817
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-09 19:25:23 +0000

    updating file for task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        fisrt = None
+        first = None
     else:
         first = sentence[:1]
     return (length, first)
