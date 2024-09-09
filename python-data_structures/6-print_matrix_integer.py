920d45e1c987c1fe5e359722f02b39b7b6673018
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-09 19:06:14 +0000

    updating file for task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(i)
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
