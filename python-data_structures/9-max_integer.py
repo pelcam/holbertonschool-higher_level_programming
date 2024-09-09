145d4a43ecc5528dedcbbf2aacd921406e5082b0
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-09 19:40:13 +0000

    updating file for task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    biggest_int = my_list[0]
+    for i in my_list:
+        if i > biggest_int:
+            biggest_int = i
+    return biggest_int
