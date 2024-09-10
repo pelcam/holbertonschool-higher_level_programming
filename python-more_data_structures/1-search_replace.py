0c55e50d6e7a928109be18d3e23958ae84a401a5
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-10 08:44:57 +0000

    updating file for task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    new_list = [replace if x==search else x for x in new_list]
+    new_list = [replace if x == search else x for x in new_list]
     return new_list
