987ab92019c39464e248d246458a7611835c1486
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-10 07:16:36 +0000

    updating file for task 9

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
+    new_list = []
     for i in new_list:
         if i < 0:
             i = -i
         modulo = i % 2
         if modulo == 0:
-            new_list[i] = True
+            new_list.append(True)
         else:
-            new_list[i] = False
+            new_list.appen(False)
     return new_list
