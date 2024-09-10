b56838008e5cdc46017e9f1e0fbe3e42faf1f2c2
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-10 08:29:00 +0000

    updating file for task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    dup_matrix = []
+    for row in matrix:
+        for i in row:
+            dup_matrix.append(i * i)
+    return dup_matrix
