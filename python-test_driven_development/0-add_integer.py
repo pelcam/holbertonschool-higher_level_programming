48760ec2909d8d16686af1f7ab362f50c8d419f8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-13 21:32:41 +0000

    updating files for task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,11 @@
-i
+#!/usr/bin/python3
+"""Contains the add_integer fonction"""
+def add_integer(a, b=98):
+    """Add two integer after convert"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
