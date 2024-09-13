7a1687c54b4a329a1a39613ccd472f5ef64f8b48
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-13 12:21:40 +0000

    updating file for task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    best_key = None
+    max_value = float('-inf')
+    if not a_dictionary:
+        return None
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            best_key = key
+    return best_key
