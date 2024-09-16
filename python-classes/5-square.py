0198007c234f5b1252172f02c53343dd3db9ee79
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-16 12:42:09 +0000

    updating file for task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""
+This module defining the Square class.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+        else:
+            for y in range(self.__size):
+                for x in range(self.__size):
+                    print('#', end='')
+                print('\n')
