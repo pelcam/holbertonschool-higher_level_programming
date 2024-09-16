e9f7210cdeeb00af3de78a6670f486b7bff4c788
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-16 14:16:15 +0000

    updating file for task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,52 @@
 #!/usr/bin/python3
+"""
+This module defining the Square class.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        if (not isinstance(position, tuple) or len(position) != 2 or
+                not all(isinstance(a, int) and a >= 0 for a in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+    
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+        else:
+            for y in range(self.__position[1]):
+                print('')
+
+            for x in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
