b79fa1733fd24467f3e6d441d758f80689d46b70
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-16 11:42:38 +0000

    updating file for task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,5 +9,9 @@ class Square:
     A class used to represent a Square.
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
