f567fb8705baea34b723d5948f4a629848fae05d
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 09:14:23 +0000

    updating file for task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,15 @@
-i
+#!/usr/bin/python3
+"""
+This module contain a function to check if
+a given object is an instance of a given class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function to check is the given object is an insatnce of the given class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
