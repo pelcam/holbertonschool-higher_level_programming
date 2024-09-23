a4f6d6dc2256406bb15b02f8be93dab5a6f3b5ef
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 08:10:15 +0000

    updating file for task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python
+"""
+This module contains a function to return the list of attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the list of available attributes and methods of an object.
+    """
+
+
+    return dir(obj)
