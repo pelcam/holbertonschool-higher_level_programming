ee28ed2da4e139c01ef8983df6055ac3dc1304c3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 09:39:55 +0000

    updating file for task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,17 @@
-i
+#!/usr/bin/python3
+"""
+This module contain a function that check if the object is an instance of a
+class that inherited (directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that check if the object is an instance of a
+    class that inherited (directly or indirectly) from the specified class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
