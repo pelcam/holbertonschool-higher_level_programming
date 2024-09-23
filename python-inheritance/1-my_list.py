c73ad3a6fab7c2e92197f9e01abd0eff903194c1
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 08:32:38 +0000

    updating file for task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python3
+"""
+This module contain the class MyList
+"""
+
+
+class MyList(list):
+    """
+    This class represent a list
+    """
+
+    def print_sorted(self):
+        self.sort()
