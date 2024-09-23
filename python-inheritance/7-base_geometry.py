30ae8dea68eafaff9f7f2b2ae35092459eee83a1
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 11:38:48 +0000

    updating file for task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,19 @@
-i
+#!/usr/bin/python3
+"""
+This module contain an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    This empty class doesn't do much for now
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+        return value
