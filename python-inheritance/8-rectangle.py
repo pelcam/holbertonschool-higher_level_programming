a37cc4c83a3b1df9f54821fec3a79d3368e2613d
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 12:51:14 +0000

    upadting files for task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+
+"""
+module containing the Rectangle class
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-This module contain the Rectangle subclass from the class BaseGeometry
-"""
 
 
 class Rectangle(BaseGeometry):
+
     """
     this class represent a rectangle, and is initiated with width an height
     """
 
     def __init__(self, width, height):
+
+        """
+        init width and height
+        """
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
