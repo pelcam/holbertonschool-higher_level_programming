3024022a7f9950b87110faccdab8c960d72ddbf8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 13:18:29 +0000

    updating file for task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,22 @@
-i
+#!/usr/bin/python3
+"""
+module with the subclass Square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class that represent a square and is inherits from the subclass Rectangle
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return '[Rectangle] {}/{}'.format(self.__size, self.__size)
