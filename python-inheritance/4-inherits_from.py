3f3a1aeb93b537d5777d821ec253590683f2e7d9
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 09:44:45 +0000

    updating file for task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,7 +11,4 @@ def inherits_from(obj, a_class):
     class that inherited (directly or indirectly) from the specified class
     """
 
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
