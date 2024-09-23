4c9c40e29303b7a1eb31dbf2342dcec22790581c
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 09:29:03 +0000

    updating file for task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,19 @@
-i
+#!/usr/bin/python3
+"""
+This module contain a function that check if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that check if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
