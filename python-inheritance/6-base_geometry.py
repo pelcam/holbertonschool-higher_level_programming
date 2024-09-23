04430cf9fff978280840e092d96c4729468719da
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-23 11:27:00 +0000

    updating file for task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,12 @@
-i
+#!/usr/bin/python3
+"""
+This module contain an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    This empty class doesn't do much for now
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
