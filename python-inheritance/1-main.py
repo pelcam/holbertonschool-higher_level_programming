535ac119a75a58e68606a9bf9fbb8b0025349e7e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-24 07:18:24 +0000

    updating file for task 1

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -10,3 +10,8 @@ my_list.append(5)
 print(my_list)
 my_list.print_sorted()
 print(my_list)
+
+lw_string = MyList(['lightweight', 'buddy'])
+print(lw_string)
+lw_string.print_sorted()
+print(lw_string)
