bcf0d2cbff80aec9f8bf0235eff9632e6871866b
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-26 16:34:34 +0000

    updating file for task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
