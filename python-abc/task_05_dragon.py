69b804264077e62bf79faa2d5a70cba192f5c14c
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-27 20:23:05 +0000

    updating file dor task 5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Module containing the Dragon class. Vive les dragons!
+"""
+
+
+class SwimMixin:
+    """
+    Mixin class that provides swimming ability.
+    """
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """
+    Mixin class that provides flying ability.
+    """
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    Dragon class. Agrougrou!
+    Inherits swimming and flying abilities from SwimMixin and FlyMixin.
+    """
+
+    def roar(self):
+        print("The dragon roars!")
