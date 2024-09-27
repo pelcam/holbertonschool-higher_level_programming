656834a795d202467ad3236bcab3b2c76f4b05c8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-27 20:12:52 +0000

    updating file for task 0

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+"""
+Module containing the Animal abstract class and concrete Dog and Cat classes.
+"""
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Abstract class for all animals.
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Method to be implemented by all subclasses to define the sound of the animal.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Dog class that implements the sound method.
+    """
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Cat class that implements the sound method.
+    """
+
+    def sound(self):
+        return "Meow"
