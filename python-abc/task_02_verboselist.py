4c08a5010c7e5c4883f7eccff4fdb74fa8e11c85
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-27 20:17:16 +0000

    updating file for task 2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Module containing the VerboseList class, which extends the built-in list with verbose output.
+"""
+
+
+class VerboseList(list):
+    """
+    VerboseList class that inherits from the built-in list and provides verbose output for list operations.
+    """
+
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
+        return item
