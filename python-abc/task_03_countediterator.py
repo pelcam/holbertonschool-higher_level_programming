0c2078ef6cedc5a82ef2fe5de4ceb3b16ef357c3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-27 20:19:00 +0000

    updating file for task 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+Module containing the CountedIterator class which tracks the number of iterations.
+"""
+
+
+class CountedIterator:
+    """
+    Iterator that tracks the number of items it has iterated over.
+    """
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        return self.count
+
+    def __iter__(self):
+        return self
