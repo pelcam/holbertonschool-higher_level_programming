2644775be55f4fdd6f45a9bbc744c4c7a6d9ad14
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 08:16:13 +0000

    updating file for task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """
+Module with the method to task 3"
 """
+import json
 
 
+def to_json_string(my_obj):
+    """
+    Method that returns the JSON representation of an object.
+    """
+
+    return json.dumps(my_obj)
