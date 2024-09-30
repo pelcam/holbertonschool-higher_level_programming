6f059a243490418b257c16c29ceeee433970d5ca
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 12:03:57 +0000

    updating file for task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,29 @@
-i
+#!/usr/bin/python3
+"""
+Module with class Student.
+"""
+
+
+class Student:
+    """
+    Class that defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        result = {}
+        for attr in attrs:
+            if attr in self.__dict__:
+                result[attr] = self.__dict__[attr]
+        return result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
