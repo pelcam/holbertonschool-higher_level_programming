a50df88d8d4f9617dd310b1749e960d92d42c867
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 08:57:44 +0000

    updating file for task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,15 @@
-i
+#!/usr/bin/python3
+"""
+Module with funciton for task 6.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that creates an Object from a “JSON file”.
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        x = f.read()
+    return json.loads(x)
