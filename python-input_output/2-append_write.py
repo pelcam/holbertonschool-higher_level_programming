dfe4712982be5775eb93470b0962de6fa31bcecc
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 08:09:55 +0000

    updating file for task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
+This module contain the method for the task 2
 """
 
 
 def append_write(filename="", text=""):
     """
+    Method to appends a string at the end of a text file
     """
 
     with open(filename, 'a', encoding="utf-8") as f:
