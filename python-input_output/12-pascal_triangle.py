4092e390ac277c2781c26f7da7f4fad4dd85d12a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 12:46:44 +0000

    updating file for task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,23 @@
-i
+#!/usr/bin/python3
+"""
+Module with function for task 12.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of lists of integers
+    representing the Pascal’s triangle of n.
+    """
+
+    global_list = []
+    if n <= 0:
+        return global_list
+
+    for x in range(n + 1):
+        list_added = []
+        for y in range(x + 1):
+            list_added.append(y)
+        global_list.append(list_added)
+
+    return global_list
