d14275fdc7b0a413a9c2244730388b3b6334333e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 08:05:24 +0000

    updating file for task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python3
+"""
+Module with a method for the task 1.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Method used to create a file and write in it.
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
