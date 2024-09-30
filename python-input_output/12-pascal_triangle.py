179149cb745eae8cb027fc750e00f8bb91b75558
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 13:17:16 +0000

    updating file for task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,15 +10,15 @@ def pascal_triangle(n):
     representing the Pascal’s triangle of n.
     """
 
-    global_list = []
+    g_list = []
     if n <= 0:
-        return global_list
+        return g_list
 
     for x in range(n):
         list_added = [1] * (x + 1)
         if x > 1:
             for y in range(1, x):
-                list_added[y] = global_list[x - 1][y - 1] + global_list[x - 1][y]
-        global_list.append(list_added)
+                list_added[y] = g_list[x - 1][y - 1] + g_list[x - 1][y]
+        g_list.append(list_added)
 
-    return global_list
+    return g_list
