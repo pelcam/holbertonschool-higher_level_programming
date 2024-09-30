b817af62cedd54b21c2120dd34eab5695958cb1a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 10:55:06 +0000

    updating file for task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module with function for task 8.
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary description
+    with simple data structure for JSON serialization of an object.
+    """
+
+    return obj.__dict__
