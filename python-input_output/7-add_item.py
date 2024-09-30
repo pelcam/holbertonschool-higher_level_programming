6678f855c978ea0e574fa8208fa9343e06bb8042
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 10:47:25 +0000

    updating file for task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,8 +8,7 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-filename = "add_items.json"
+filename = "add_item.json"
 if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
