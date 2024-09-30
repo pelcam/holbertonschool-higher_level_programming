d9803a3b7c001b03fea0359085fd35c24101f4f2
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 09:35:06 +0000

    updating file for task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,18 @@
-i
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list, and then save them to a file.
+"""
+import json
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_items.json"
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
