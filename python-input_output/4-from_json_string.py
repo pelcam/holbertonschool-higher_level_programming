93038cea766f5b65d0846322e460c9095b808945
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 08:22:00 +0000

    updating file for task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python3
+"""
+Module with function for task 4.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that returns an object represented by a JSON string
+    """
+
+    return json.loads(my_str)
