bff584e61c3ceb1dd43f66af62466f1f0635e2db
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 11:34:16 +0000

    updating file for task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,18 @@
-i
+#!/usr/bin/python3
+"""
+Module with class Student.
+"""
+
+
+class Student:
+    """
+    Class that defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
