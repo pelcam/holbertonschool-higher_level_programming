aa72249a7fef720dc48631c687e48a9a0a4f826e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-30 07:56:58 +0000

    updating file for task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contain aa method to print a file to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Method used to read and print a file to stdout.
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
