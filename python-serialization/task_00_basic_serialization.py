e053d3bd2d9913ccbfc73b81a2ae8739af378791
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-01 11:18:55 +0000

    updating file for task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,19 @@
-i
+#!/usr/bin/python3
+"""
+Module with the functions for task 0
+"""
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Function that serialize and save data to the specified file
+    """
+
+    pass
+
+def load_and_deserialize(filename):
+    """
+    Function that load and deserialize data from the specified file
+    """
+
+    pass
