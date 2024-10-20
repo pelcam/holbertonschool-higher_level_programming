85b965ff6cb0d4e0a05702d4df1cecc374fced20
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-20 19:20:59 +0000

    updating file for task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,1 +1,43 @@
-i
+#!/usr/bin/python3
+"""
+Module wihth a class to check the server with a simple API
+"""
+
+
+import json
+import http.server
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status_data = {"status": "OK"}
+            self.wfile.write(json.dumps(status_data).encode())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode())
+
+
+PORT = 8000
+
+
+with http.server.HTTPServer(("", PORT), SimpleAPIHandler) as httpd:
+    httpd.serve_forever()
