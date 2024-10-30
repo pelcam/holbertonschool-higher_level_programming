350b929f37add14c90519d2f4c0b826977b0edd4
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-30 10:28:33 +0000

    updating file for task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,1 +1,4 @@
-i
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in the MySQL server
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score;
