a0aa8cfcdf29dd38911d82c0d552fb28af0b0d2f
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-10-31 10:05:44 +0000

    updating file for task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+USE hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
