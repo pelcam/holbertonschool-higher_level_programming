9081d93df6315535fc4492feb0c4f9580d150dbb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-02 10:40:05 +0000

    updating file for task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    for state in cursor.fetchall():
+        if str(state[1]).startswith("N"):
+            print(state)
+    cursor.close()
+    db.close()
