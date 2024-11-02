f30db541c360e266f726b394398d5acd5345971b
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-02 11:20:56 +0000

    updating file for task

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''
+takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa
+'''
+""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+        JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC;", (sys.argv[4], ))
+    print(", ".join([row[0] for row in cursor.fetchall()]))
+    cursor.close()
+    db.close()
