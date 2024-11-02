5f6537a7a90413d64adb2510de6b3dac2b148508
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-02 10:34:19 +0000

    updating file for task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """
+lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="hbtn_0e_0_usa"
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
