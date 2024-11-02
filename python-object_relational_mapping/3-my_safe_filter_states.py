8f9fe0228b579d30dddd1b5b7dcb4e711d53b7d0
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-02 10:56:16 +0000

    updating file for task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+But this time, this one is safe from MySQL injections !
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    for state in cursor.fetchall():
+        if str(state[1]) == sys.argv[4]:
+            print(state)
+    cursor.close()
+    db.close()
