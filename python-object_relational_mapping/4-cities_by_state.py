98e50f4f69c7a94699b267fcda5ce66f7899efe4
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-11-02 11:09:43 +0000

    updating file for task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         database=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;")
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;")
     for state in cursor.fetchall():
         print(state)
     cursor.close()
